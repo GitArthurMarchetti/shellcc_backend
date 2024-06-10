bec489a62a0661efa0322cee0ac88e88a29f0e9f
Arthur D´eça Marchetti <avinhasmarchetti@gmail.com>
2024-06-10 15:38:10 -0300

    Add files via upload

---
+++ shellcc/src/main/java/com/sa/shellcc/ShellccApplication.java
@@ -0,0 +1,13 @@
+package com.sa.shellcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShellccApplication.class, args);
+	}
+
+}
