162afe214b8820b65c93a2464695d06b5a567c3a
Arthur D´eça Marchetti <avinhasmarchetti@gmail.com>
2024-06-10 15:42:46 -0300

    Create Java SpringBoot

---
+++ shellcc/src/test/java/com/sa/shellcc/ShellccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa.shellcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShellccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
