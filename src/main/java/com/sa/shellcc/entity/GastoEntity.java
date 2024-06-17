498d59cad124506ef04bafbbe86d044e20fa890d
duduborges <duduborges333969@gmail.com>
2024-06-17 16:01:40 -0300

    adicionado gastoEntity

--- src/main/java/com/sa/shellcc/entity/GastoEntity.java
+++ src/main/java/com/sa/shellcc/entity/GastoEntity.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.sa.shellcc.entity;
 
-/**
- *
- * @author eduardoborges
- */
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="gastos")
 public class GastoEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long IdGasto;
+    
+    @Column(nullable=false)
+    private String nome;
+    
+    @Column(nullable=true)
+    private String descricao;
 }
