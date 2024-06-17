6728ece0719ad5801ecb73b3ff0c043deaf9449e
Arthur D´eça Marchetti <avinhasmarchetti@gmail.com>
2024-06-17 16:05:06 -0300

    Entity Usuario criado. APPLICATION.PROPERTIES atualizado

--- src/main/java/com/sa/shellcc/entity/UsuarioEntity.java
+++ src/main/java/com/sa/shellcc/entity/UsuarioEntity.java
@@ -1,6 +1,71 @@
 
 package com.sa.shellcc.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class UsuarioEntity {
+       
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    
+    @Column(nullable=false)
+    private String email;
+    
+    @Column(nullable=false)
+    private String senha;
+     
+    @Column(nullable=false)
+    private String tokkenSenha;
+     
+    @Column(nullable=false)
+    private Integer cargo;
+    
+    //------------------------------G&S----------------------------------------
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTokkenSenha() {
+        return tokkenSenha;
+    }
+
+    public void setTokkenSenha(String tokkenSenha) {
+        this.tokkenSenha = tokkenSenha;
+    }
+
+    public Integer getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Integer cargo) {
+        this.cargo = cargo;
+    }
+    
+    
     
 }
