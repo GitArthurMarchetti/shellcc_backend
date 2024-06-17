0d81edfe650f99709f6abf1f86680822b7ff417b
duduborges <duduborges333969@gmail.com>
2024-06-17 16:02:48 -0300

    adicionado gastoEntity

--- src/main/java/com/sa/shellcc/entity/GastoEntity.java
+++ src/main/java/com/sa/shellcc/entity/GastoEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Date;
 
 
 @Entity
@@ -22,4 +23,77 @@ public class GastoEntity {
     
     @Column(nullable=true)
     private String descricao;
+    
+    @Column(nullable=false)
+    private Double preco;
+    
+    @Column(nullable=false)
+    private Date dataGasto;
+    
+    @Column(nullable=false)
+    private boolean repeticao;
+    
+    @Column(nullable=true)
+    private String opcao;
+    
+    
+    //---------------------------GET--AND--SETTERS-----------------------------
+
+    public Long getIdGasto() {
+        return IdGasto;
+    }
+
+    public void setIdGasto(Long IdGasto) {
+        this.IdGasto = IdGasto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Date getDataGasto() {
+        return dataGasto;
+    }
+
+    public void setDataGasto(Date dataGasto) {
+        this.dataGasto = dataGasto;
+    }
+
+    public boolean isRepeticao() {
+        return repeticao;
+    }
+
+    public void setRepeticao(boolean repeticao) {
+        this.repeticao = repeticao;
+    }
+
+    public String getOpcao() {
+        return opcao;
+    }
+
+    public void setOpcao(String opcao) {
+        this.opcao = opcao;
+    }
+    
+    
 }
