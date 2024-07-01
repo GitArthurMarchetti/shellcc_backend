5c857d554bac00de61e85a7039deeb0d3b7163ce
duduborges <duduborges333969@gmail.com>
2024-07-01 16:25:56 -0300

    atualizando

--- src/main/java/com/sa/shellcc/controller/UsuarioController.java
+++ src/main/java/com/sa/shellcc/controller/UsuarioController.java
@@ -1,7 +1,13 @@
 
 package com.sa.shellcc.controller;
 
+import com.sa.shellcc.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsuarioController {
-    
+    @Autowired
+    private UsuarioService usuarioService;
     
 }
